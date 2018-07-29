23a04d0d197d7fb5759d5e6751e102ba1baf8de7
Econa77 <s.f.1992.ip@gmail.com>
2018-07-30 03:56:59 +0900

    Implement mapping keyboard layout sources and key codes

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//
+//  Sauce
+//  GitHub: https://github.com/clipy
+//  HP: https://clipy-app.com
+//
+//  Created by Econa77 on 2018/07/30.
+//
+//  Copyright © 2018 Clipy Project.
+//
+
+import Cocoa
+
+@NSApplicationMain
+final class AppDelegate: NSObject, NSApplicationDelegate {
+
+    // MARK: - NSApplication Delegate
+    func applicationDidFinishLaunching(_ aNotification: Notification) {}
+
+    func applicationWillTerminate(_ aNotification: Notification) {}
+
+}
+
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//
+//  Sauce
+//  GitHub: https://github.com/clipy
+//  HP: https://clipy-app.com
+//
+//  Created by Econa77 on 2018/07/30.
+//
+//  Copyright © 2018 Clipy Project.
+//
+
+import Cocoa
+import Sauce
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        _ = Sauce.shared
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        print(Sauce.shared.currentKeyCode(by: .v))
+    }
+
+}
+
